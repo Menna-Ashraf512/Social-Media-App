1a5b7b97a809422502b591079d5570b874393681
Menna-Ashraf512 <mennaalla@gmail.com>
2025-03-07 22:32:57 +0200

    implement post service and interfaces for handling posts

--- src/app/core/services/posts/posts.service.ts
+++ src/app/core/services/posts/posts.service.ts
@@ -1,12 +1,27 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { baseUrl } from '../../environment/Base_Url';
+import { IPost } from '../../interfaces/posts/ipost';
+import { IAllPost } from '../../interfaces/posts/iallposts';
+import { IPostById } from '../../interfaces/posts/ipost-by-id';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class PostsService {
+  endPoint = `posts`
 
   constructor(private readonly _httpClient:HttpClient) { }
 
-  
+  createPost(postData:FormData){
+    this._httpClient.post<IPost>(`${baseUrl}${this.endPoint}`,postData)
+  }
+  getAllPosts(){
+    this._httpClient.get<IAllPost>(`${baseUrl}${this.endPoint}`)
+  }
+  getPostById(postId:string){
+    this._httpClient.get<IPostById>(`${baseUrl}${this.endPoint}/${postId}`)
+  }
 }
